e5ff7f820fe26bfb5857828807eeb234e0f27263
Elliot Shepherd <elliot@jarofworms.com>
2014-03-02 22:41:55 +1100

    Some initial examples of services, states and protocols

---
+++ state/common.js
@@ -0,0 +1,10 @@
+{
+  "id": "http://ninjablocks.com/schema/state/common",
+  "description": "Common NinjaBlocks state definitions",
+  "definitions": {
+    "number-timeseries": {
+      "description": "Used for a state object, it indicates that this property should be stored in the time-series database.",
+      "type": "number"
+    }
+  }
+}
